f84f54d606da149eadc146d5784137103edfae77
NaSuRall <romain.poulain@edu.esiee-it.fr>
2026-02-03 14:45:07 +0000

    ADD Link on main and Folder to Create Update Show and Delete Users

---
+++ src/handlers/user/mod.rs
@@ -0,0 +1,12 @@
+pub async fn get_all_user() -> String {
+    "Hello, user!".to_string()
+}
+pub async fn create_user() -> String {
+    "Hello, user!".to_string()
+}
+pub async fn update_user() -> String {
+    "Hello, user!".to_string()
+}
+pub async fn delete_user() -> String {
+    "Hello, user!".to_string()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use sqlx::MySqlPool;
+
+mod handlers;
+
+use crate::handlers::user::{create_user, delete_user, get_all_user, update_user};
+
+#[tokio::main]
+async fn main() {
+    // Lien pour se connecter a la base de données
+    let database_url =
+        "mysql://u998935179_apiRust:TTrcbt12@193.203.168.188:3306/u998935179_apiRust";
+
+    // Création de la pool de connexion à la base de données
+    let pool = MySqlPool::connect(database_url)
+        .await
+        .expect("Failed to connect to Mysql");
+
+    // Rooter de l'API ici :
+    let app = Router::new()
+        .route("/", get(|| async { "Hello world" }))
+        .route(
+            "/users",
+            get(get_all_user)
+                .post(create_user)
+                .put(update_user)
+                .delete(delete_user),
+        )
+        .with_state(pool);
+
+    // Lancement du serveur
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on http://{}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
